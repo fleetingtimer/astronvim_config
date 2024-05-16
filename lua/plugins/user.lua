1b80a5f00dfdaa6111add71769fdc2df277c6f7b
fleetingtimer <fleetingtimer@fleetinimersMBP.lan>
2024-05-16 23:56:20 +0800

    feat: add spectre keymaps

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -4,6 +4,8 @@
 -- Here are some examples:
 
 ---@type LazySpec
+local leet_arg = "leetcode.nvim"
+
 return {
 
   -- == Examples of Adding Plugins ==
@@ -164,6 +166,18 @@ return {
     },
     config = function()
       require("spectre").setup()
+      vim.api.nvim_set_keymap('n', '<leader>ss', '<cmd>lua require("spectre").toggle()<CR>', {
+        desc = "Toggle Spectre"
+      })
+      vim.api.nvim_set_keymap('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+        desc = "Search current word"
+      })
+      vim.api.nvim_set_keymap('v', '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+        desc = "Search current word"
+      })
+      vim.api.nvim_set_keymap('n', '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+        desc = "Search on current file"
+      })
     end,
   }
 }
